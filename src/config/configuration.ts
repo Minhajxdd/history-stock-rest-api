601de1941463f4578a1bb38bdeb13974a85bebd2
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-26 15:53:23 +0530

    feat(cofig): date from environemnets done

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -4,4 +4,8 @@ export default () => ({
     credential_path: process.env.CREDENTIALS_PATH,
     google_sheets_scopes: process.env.GOOGLE_SHEETS_SCOPES || '',
   },
+  date: {
+    start_date: process.env.START_DATE || '',
+    end_date: process.env.END_DATE || '',
+  },
 });
