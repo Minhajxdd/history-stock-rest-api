feb0a5c68620499f3554d115f77e14c7e6b26372
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-26 15:33:12 +0530

    feat(stock): working of stock is done

--- src/stock/Controllers/stock.controller.ts
+++ src/stock/Controllers/stock.controller.ts
@@ -4,14 +4,14 @@ import { getStockPriceDto } from '../Dto/get-stock-price.dto';
 
 @Controller('api')
 export class StockController {
-  constructor(private readonly stockService: StockService) {}
-
-  // /api/stock-price?symbol={symbol }&date={YYYY-MM-DD}
+  constructor(private readonly _stockService: StockService) {}
 
   @Get(`stock-price`)
   getStockPrice(
     @Query(new ValidationPipe({ transform: true })) query: getStockPriceDto,
   ) {
-    return query;
+    const { date, symbol } = query;
+
+    return this._stockService.getStockPrice(date, symbol);
   }
 }
--- src/stock/Services/stock.service.ts
+++ src/stock/Services/stock.service.ts
@@ -1,4 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { GSheetUtils } from '../Utils/g-sheets.utils';
+import { FormatUtils } from '../Utils/format.utils';
+import { google } from 'googleapis';
 
 @Injectable()
-export class StockService {}
+export class StockService {
+  constructor(
+    private readonly _gSheetUtils: GSheetUtils,
+    private readonly _formatUtils: FormatUtils,
+  ) {}
+
+  async getStockPrice(date: string, symbol: string): Promise<any> {
+    const headers = await this._gSheetUtils.getHeaders();
+
+    const columnIndex = headers.indexOf(symbol);
+
+    if (columnIndex === -1) {
+      throw new BadRequestException('Symbol not found');
+    }
+
+    const rowNumber = this._formatUtils.dateToRowNumber(date);
+
+    if (!rowNumber) {
+      throw new BadRequestException(
+        `Date out of range (2024-04-01 to 2025-03-31)`,
+      );
+    }
+
+    const auth = this._gSheetUtils.getAuth();
+    const sheets = google.sheets({ version: 'v4', auth });
+
+    const range = `Sheet1!${String.fromCharCode(65 + columnIndex)}${rowNumber}`;
+
+    const response = await sheets.spreadsheets.values.get({
+      spreadsheetId: process.env.GOOGLE_SHEETS_ID,
+      range: range,
+    });
+
+    if (!response.data.values || !response.data.values[0]) {
+      throw new BadRequestException('Price data not found');
+    }
+
+    const data = {
+      symbol,
+      date,
+      price: response.data.values[0][0],
+    };
+
+    return {
+      status: 'success',
+      message: 'successfully fetched data',
+      data,
+    };
+  }
+}
