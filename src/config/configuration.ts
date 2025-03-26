c50d37d979a2b1c148cbde27f3a3aaeb189bfad9
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-26 15:03:17 +0530

    feat(config): google sheets scope added in configuration

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -2,5 +2,6 @@ export default () => ({
   google_sheets: {
     google_sheet_id: process.env.GOOGLE_SHEETS_ID,
     credential_path: process.env.CREDENTIALS_PATH,
+    google_sheets_scopes: process.env.GOOGLE_SHEETS_SCOPES || '',
   },
 });
