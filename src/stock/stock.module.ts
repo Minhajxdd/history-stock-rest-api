190def57fffb99eb7ee0c9d0a61dd313d4a6a304
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-26 14:45:08 +0530

    feat(gsheetutils): registered in stock module

--- src/stock/stock.module.ts
+++ src/stock/stock.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { StockService } from './Services/stock.service';
 import { StockController } from './Controllers/stock.controller';
+import { GSheetUtils } from './Utils/g-sheets.utils';
 
 @Module({
   controllers: [StockController],
-  providers: [StockService],
+  providers: [StockService, GSheetUtils],
 })
 export class StockModule {}
