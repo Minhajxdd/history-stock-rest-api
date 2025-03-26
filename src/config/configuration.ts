a692b6fb1b36d4c888135383a6bf6aaee112e69f
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-26 14:42:28 +0530

    feat(config): config google imports done

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,1 +1,6 @@
-export default () => ({});
+export default () => ({
+  google_sheets: {
+    google_sheet_id: process.env.GOOGLE_SHEETS_ID,
+    credential_path: process.env.CREDENTIALS_PATH,
+  },
+});
