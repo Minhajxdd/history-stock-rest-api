fd86ed575139de82ea2c0aa398e52ededa946dc7
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-26 15:13:57 +0530

    feat(format): format stock module registered

--- src/stock/stock.module.ts
+++ src/stock/stock.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { StockService } from './Services/stock.service';
 import { StockController } from './Controllers/stock.controller';
 import { GSheetUtils } from './Utils/g-sheets.utils';
+import { FormatUtils } from './Utils/format.utils';
 
 @Module({
   controllers: [StockController],
-  providers: [StockService, GSheetUtils],
+  providers: [StockService, GSheetUtils, FormatUtils],
 })
 export class StockModule {}
